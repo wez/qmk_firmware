ff769c749c37564eb061223cb89ceb8d558505f6
Wez Furlong <wez@fb.com>
2016-10-11 19:25:15 -0700

    muscle memory wants grave in the bottom left from my ergodox, but also wants FN down there from my macbook for the arrow keys and home/end

--- keyboards/atomic/keymaps/wez/keymap.c
+++ keyboards/atomic/keymaps/wez/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+- 2u ---------------------+--------|
  * | SFT/CAP| Z      | X      | C      | V      | B      | N      | M      | ,      | .      | /      | XXXXXX . RSHIFT | UP     | PG DN  |
  * |--------+--------+--------+--------+--------+- 2u ------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | FN/MDIA  | LCTRL     | LGUI     |    ALT   | XXXXXX . SPACE  | RGUI     | RALT     | RCTRL    | FN/MDIA   | LEFT   | DOWN   | RIGHT  |
+ * | `/MDIA   | LCTRL     | LGUI     |    ALT   | XXXXXX . SPACE  | RGUI     | RALT     | RCTRL    | FN/MDIA   | LEFT   | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 [BASE] = {
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC, KC_RBRC,KC_BSLS, KC_DEL},
   {CTL_T(KC_ESC),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT, KC_ENT, KC_ENT,  KC_PGUP},
   {TD(TD_SFT_CAPS),KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT, KC_RSFT,KC_UP,   KC_PGDN},
-  {F(FN_MDIA_TOG), KC_LCTL,KC_NO,  KC_LGUI,KC_LALT,KC_SPC, KC_SPC, KC_RGUI,KC_RALT,KC_NO,  KC_RCTL,          F(FN_MDIA_TOG),KC_LEFT, KC_DOWN,KC_RIGHT},
+  {LT(MDIA,KC_GRV),KC_LCTL,KC_NO,  KC_LGUI,KC_LALT,KC_SPC, KC_SPC, KC_RGUI,KC_RALT,KC_NO,  KC_RCTL,          F(FN_MDIA_TOG),KC_LEFT, KC_DOWN,KC_RIGHT},
 },
 /* Keymap MDIA: Media and mouse keys
  * .---------------------------------------------------------------------------------------------------------------------- 2u ------------.

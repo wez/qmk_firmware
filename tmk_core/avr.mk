294144af1d5bc2ab1890799c9a3b6d64dd961e2c
Wez Furlong <wez@wezfurlong.org>
2018-08-29 17:56:12 -0700

    use avrdude available on my linux system

--- tmk_core/avr.mk
+++ tmk_core/avr.mk
@@ -107,7 +107,7 @@ flip: $(BUILD_DIR)/$(TARGET).hex
 	batchisp -hardware usb -device $(MCU) -operation start reset 0
 
 avrdude: $(BUILD_DIR)/$(TARGET).hex sizeafter
-	while ! /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -patmega32u4 -cavr109 -b57600 -D -Uflash:w:$(BUILD_DIR)/$(TARGET).hex:i ; do sleep 1 ; done
+	while ! avrdude -v -patmega32u4 -cavr109 -b57600 -P/dev/ttyACM0 -D -Uflash:w:$(BUILD_DIR)/$(TARGET).hex:i ; do sleep 1 ; done
 
 dfu: $(BUILD_DIR)/$(TARGET).hex sizeafter
 	until dfu-programmer $(MCU) get bootloader-version; do\

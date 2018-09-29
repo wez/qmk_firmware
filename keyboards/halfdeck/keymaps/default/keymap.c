e6fe31a35ded9a4429e06537365723876e45b2f9
Wez Furlong <wez@wezfurlong.org>
2018-09-29 09:03:42 -0700

    swap ctrl and alt around to match the flutterby

--- keyboards/halfdeck/keymaps/default/keymap.c
+++ keyboards/halfdeck/keymaps/default/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CTL_T(KC_ESC), KC_A,     KC_S,         KC_D,     KC_F,      KC_G,     KC_MINS,
     KC_LSFT,       KC_Z,     KC_X,         KC_C,     KC_V,      KC_B,
                                                                           KC_LGUI,
-                   KC_VOLU,                                     KC_LALT,  KC_LCTRL,
+                   KC_VOLU,                                     KC_LCTRL, KC_LALT,
     MO(RAISE),     KC_VOLD,  KC_PSCREEN,                        MO(RAISE),KC_BSPC,
 
     // RIGHT
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL,     KC_H,     KC_J,     KC_K,      KC_L,       KC_SCLN, KC_QUOT,
                 KC_N,     KC_M,     KC_COMM,   KC_DOT,     KC_SLSH, KC_RSFT,
     KC_RGUI,
-    KC_RCTRL,   KC_RALT,                                   KC_UP,
+    KC_RALT,    KC_RCTRL,                                  KC_UP,
     KC_ENT,     KC_SPC,                        KC_LEFT,    KC_DOWN, KC_RIGHT
     ),
 
